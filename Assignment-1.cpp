8e843b9099c1e03d21a12023b6ef92553325f362
Kazi555 <kazisohail555@gmail.com>
2022-12-13 15:51:40 +0530

    first commit

---
+++ Assignment-1.cpp
@@ -0,0 +1,20 @@
+// Swapping the two value
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+    int c;
+
+    c = a;
+    a = b;
+    b = c;
+
+    cout << a;
+    cout << b;
+
+    return 0;
+}
