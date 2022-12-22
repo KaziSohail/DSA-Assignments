a5d8d92343153d9d2c8a9d0d339afc7d55e6eee6
Kazi555 <kazisohail555@gmail.com>
2022-12-22 15:20:11 +0530

    LEC9.1

---
+++ LEC-9-01-Bubble-Sort-Array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[] = {6, 5, 4, 3, 2, 1};
+    int n = (sizeof(a)) / (sizeof(int));
+    int temp;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - (1 + i); j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << "\t";
+    }
+
+    return 0;
+}
